fc781b191c88c5053b7d969472dc68d32618f5aa
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-11 20:48:46 -0600

    Write test for get_valid_plays

--- elevens_simulator/src/main.rs
+++ elevens_simulator/src/main.rs
@@ -1,3 +1,48 @@
+mod card;
+mod game;
+
+use card::{Play, Card, NumberedCard, FaceCard};
+use game::Game;
+
 fn main() {
-  println!("Hello, world!");
+  let mut game = Game::new();
+  game.init();
+  println!("{:#?}", game.board.cards);
+  println!("{:#?}", game.get_valid_plays());
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn get_valid_plays() {
+    let mut game = Game::new();
+    game.init();
+    game.board.cards = vec![
+      Card::Number(NumberedCard::One),
+      Card::Number(NumberedCard::Two),
+      Card::Number(NumberedCard::Three),
+      Card::Number(NumberedCard::Four),
+      Card::Number(NumberedCard::Six),
+      Card::Number(NumberedCard::Eight),
+      Card::Number(NumberedCard::Nine),
+      Card::Number(NumberedCard::Ten),
+      Card::Face(FaceCard::Jack),
+      Card::Face(FaceCard::Queen),
+      Card::Face(FaceCard::King),
+    ];
+
+    assert_eq!(game.get_valid_plays(), vec![
+      Play::NumberedPair(NumberedCard::One),
+      Play::NumberedPair(NumberedCard::Two),
+      Play::NumberedPair(NumberedCard::Three),
+      Play::NumberedPair(NumberedCard::Eight),
+      Play::NumberedPair(NumberedCard::Nine),
+      Play::NumberedPair(NumberedCard::Ten),
+      Play::FaceTriple,
+      Play::FaceTriple,
+      Play::FaceTriple
+    ]);
+  }
 }
